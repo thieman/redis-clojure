5da832a84357091abafc11d26d1f2d4433014d57
tthieman <travis.thieman@gmail.com>
2013-10-01 14:22:42 -0400

    All five request types work! Sorta! Mostly!

--- src/redis_clojure/core.clj
+++ src/redis_clojure/core.clj
@@ -2,6 +2,8 @@
   (:import (java.net Socket)
            (java.io PrintWriter InputStreamReader BufferedReader)))
 
+(def debug true)
+
 (declare conn-handler append-status-reply append-error-reply append-integer-reply
          append-bulk-reply append-multi-bulk-reply append-complete-response)
 
@@ -19,7 +21,7 @@
     (doto (Thread. #(conn-handler conn)) (.start))
     conn))
 
-(defn write [conn msg]
+(defn- write [conn msg]
   (doto (:out @conn)
     (.println (str msg "\r"))
     (.flush)))
@@ -30,19 +32,19 @@
         args-list (reduce specify-arg [] args)]
     (clojure.string/join "\r\n" (flatten (conj [] (str "*" total-args) args-list "")))))
 
-(defn spec-int-from-response-part [response-part]
+(defn- spec-int-from-response-part [response-part]
   (-> response-part
       (next)
       ((partial drop-last 2))
       (clojure.string/join)
       (Integer/parseInt)))
 
-(defn bulk-is-complete? [response-parts]
+(defn- bulk-is-complete? [response-parts]
   (let [response-size (spec-int-from-response-part (first response-parts))
         content (clojure.string/join (next response-parts))]
     (= (+ 2 response-size) (count content))))
 
-(defn consolidate-multi-bulk-args [args]
+(defn- consolidate-multi-bulk-args [args]
   (loop [args args
          result []
          expected-size nil]
@@ -56,15 +58,26 @@
             (recur (next args) (conj result x) nil)
             (if (or (= \* (first (last result))) (= \$ (first (last result))))
               (recur (next args) (conj result x) expected-size)
-              (recur (next args) (conj (vec (butlast result)) (clojure.string/join (last result) x)) expected-size))))))))
+              (recur (next args) (conj (vec (butlast result)) (clojure.string/join [(last result) x])) expected-size))))))))
 
-(defn multi-bulk-is-complete? [response-parts]
+(defn- gen-arg-pairs [multi-args]
+  (loop [result []
+         args (next multi-args)]
+    (if (zero? (count args))
+      result
+      (let [current (if (<= 2 (count result)) (vec (butlast result)) [])]
+        (if (zero? (rem (count (last result)) 2))
+          (recur (conj result [(first args)]) (next args))
+          (recur (conj current (conj (last result) (first args))) (next args)))))))
+
+(defn- multi-bulk-is-complete? [response-parts]
   (let [total-args (spec-int-from-response-part (first response-parts))
-        consolidate-args (fn [x] (if (= \$ (first x)))))))
+        consolidated-args (consolidate-multi-bulk-args response-parts)
+        arg-pairs (gen-arg-pairs consolidated-args)]
+    (and (= (+ 1 (* 2 total-args)) (count consolidated-args))
+         (every? true? (map bulk-is-complete? arg-pairs)))))
 
-
-
-(defn is-complete? [response-parts]
+(defn- is-complete? [response-parts]
   (cond
    (contains? #{:status :error :integer} @response-type)
    true
@@ -79,12 +92,12 @@
   (let [responses-size (count @responses)]
     (write conn msg)
     (while (= responses-size (count @responses))
-      (Thread/sleep 1)
+      (Thread/sleep 1))
     (let [result (first @responses)]
       (reset! responses [])
       result)))
 
-(defn finalize-current-response []
+(defn- finalize-current-response []
   (let [current @current-response
         head (-> (drop-last 2 (next (first current)))
                  (clojure.string/join))]
@@ -95,35 +108,40 @@
       :bulk (append-bulk-reply current)
       :multi-bulk (append-multi-bulk-reply current))))
 
-(defn append-status-reply [msg]
-  (append-complete-response {:type "status"
+(defn- append-status-reply [msg]
+  (append-complete-response {:type :status
                              :value (msg)}))
 
-(defn append-error-reply [msg]
-  (append-complete-response {:type "error"
+(defn- append-error-reply [msg]
+  (append-complete-response {:type :error
                              :value (msg)}))
 
-(defn append-integer-reply [msg]
-  (append-complete-response {:type "integer"
+(defn- append-integer-reply [msg]
+  (append-complete-response {:type :integer
                              :value (Integer/parseInt msg)}))
 
-(defn append-bulk-reply [msg]
-  (println "bulk reply"))
+(defn- append-bulk-reply [msg]
+  (append-complete-response {:type :bulk
+                             :value (map (comp clojure.string/join (partial drop-last 2)) (next msg))}))
 
-(defn append-multi-bulk-reply [msg]
-  (println "multi-bulk reply"))
+(defn- append-multi-bulk-reply [msg]
+  (let [value (map (comp clojure.string/join (partial drop-last 2)) (next msg))]
+    (append-complete-response {:type :multi-bulk
+                               :value (filter (complement nil?) (map-indexed #(if (odd? %1) %2 nil) value))})))
 
-(defn append-response-part [msg]
+(defn- append-response-part [msg]
   (swap! current-response #(conj % msg))
   (when (= @response-type :none)
     (reset! response-type (get response-types (first msg))))
   (when (is-complete? @current-response)
     (finalize-current-response)))
 
-(defn append-complete-response [response]
-  (swap! responses #(conj % response)))
+(defn- append-complete-response [response]
+  (swap! responses #(conj % response))
+  (reset! response-type :none)
+  (reset! current-response []))
 
-(defn conn-handler [conn]
+(defn- conn-handler [conn]
   (loop [msg ""]
     (let [in-char (char (.read (:in @conn)))
           curr-msg (str msg in-char)]
@@ -137,5 +155,7 @@
           (recur curr-msg))))))
 
 (defn -main []
-  (let [conn (connect "127.0.0.1" (int 6379))]
-    (println (synchronous-request conn "*2\r\n$4\r\nLLEN\r\n$5\r\nBACON\r\n"))))
+  (let [conn (connect "127.0.0.1" 6379)]
+    (println (synchronous-request conn (request-string "llen" "bacon")))
+    (println (synchronous-request conn (request-string "lrange" "bacon" 0 1)))))
+    ;; (println (synchronous-request conn (request-string "info")))))
